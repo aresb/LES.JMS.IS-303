849064bdac4eeb641158637ab82028e695d90879
Are Småge Breidablikk <aaresb@hotmail.com>
2012-04-11 11:26:06 +0200

    Byttet JNDI med Resources-annotasjoner

--- jms.test/jms.test-war/src/java/ObserverGateway.java
+++ jms.test/jms.test-war/src/java/ObserverGateway.java
@@ -10,6 +10,8 @@ import javax.jms.TextMessage;
 import javax.naming.NamingException;
 import java.util.Observer;
 import java.util.Observable;
+import javax.annotation.Resource;
+import javax.jms.*;
 
 
 public class ObserverGateway implements MessageListener {
@@ -18,6 +20,12 @@ public class ObserverGateway implements MessageListener {
 	private Observer observer;
 	private Connection connection;
 	private MessageConsumer updateConsumer;
+                
+        @Resource(name="connFactory", mappedName="webTrackerConnFactory")
+        private QueueConnectionFactory qFactory;
+
+        @Resource(name="jmsQueue", mappedName="webTrackerQueue")
+        private Queue queue;
 
 	protected ObserverGateway() {
 		super();
@@ -31,12 +39,11 @@ public class ObserverGateway implements MessageListener {
 	}
 
 	protected void initialize(Observer observer) throws JMSException, NamingException {
-		this.observer = observer;
-                JNDIUtil jndi = new JNDIUtil();
-		ConnectionFactory connectionFactory = jndi.getConnectionFactory(jndi.factoryName);
+		this.observer = observer;   
+		ConnectionFactory connectionFactory = qFactory;
 		connection = connectionFactory.createConnection();
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		Destination updateTopic = jndi.getDestination(UPDATE_TOPIC_NAME);
+		Destination updateTopic = queue;
 		updateConsumer = session.createConsumer(updateTopic);
 		updateConsumer.setMessageListener(this);
 	}
@@ -61,7 +68,7 @@ public class ObserverGateway implements MessageListener {
 			connection.close();
 		}
 	}
-
+        //TODO: Denne metoden skal hente oppdateringer fra SubjectGateway
 	private void update(String newState) throws JMSException {
             Observable o = null;
             Object arg = null;
